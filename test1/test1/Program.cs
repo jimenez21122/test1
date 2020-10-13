f210f67a9e1ce9c571e9483cb3188cd4913e3d80
jimenez21122 <71835827+jimenez21122@users.noreply.github.com>
2020-10-13 19:58:32 +0200

    fibonacci
    terminar

--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -6,11 +6,11 @@ namespace test1
     {
         static void Main(string[] args)
         {
-            int i = 2;
-            while (i < 100 )
-            {
-                Ejercicios.Ejercicio3(i);
-            }
+            Ejercicios.Ejercicio8();
+
+                
+
+           
 
             
 
